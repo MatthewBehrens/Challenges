78377b821e66755099584d41855372beeda87c85
Matthew Behrens <mattbehrens96@gmail.com>
2016-11-09 09:19:11 -0600

    finish kaprekars challenge

--- Kaprekars_Routine/kaprekars.rb
+++ Kaprekars_Routine/kaprekars.rb
@@ -1,9 +1,29 @@
-def largest_digit(number)
-	array = number.to_s.chars.map(&:to_i)
-	array.max
+def num_to_arr(num)
+  num.to_s.split(//)
 end
 
-def desc_digits(number)
-	array = number.to_s.chars.map(&:to_i)
-	array.sort.reverse.join.to_i
+
+def largest_digit(num)
+	array = num.to_s.chars.map(&:to_i)
+	if array.length > 4
+		return false
+	else
+		return array.max
+	end
+end
+
+def desc_digits(num, desc=true)
+  arr = num_to_arr(num)
+  (4-arr.size).times { arr.unshift("0") }
+  arr = desc ? arr.sort.reverse : arr.sort
+  arr.join.to_i
+end
+
+def kaprekar(num)
+  counter = 0
+  until num == 6174
+    num = desc_digits(num) - desc_digits(num, false)
+    counter += 1
+  end
+  counter
 end
